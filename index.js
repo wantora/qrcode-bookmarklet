1316f0c62cd52a68e9552999868a1c406b1e1d8f
wantora <wantora@gmail.com>
2016-11-05 23:28:52 +0900

    Add input element.

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 "use strict";
 
 const qrcode = require("qrcode-generator");
-
-const data = location.href;
 const errorCorrectionLevel = "L";
-let qr;
-let typeNumber = 1;
 
-while (typeNumber <= 40) {
-  qr = qrcode(typeNumber, errorCorrectionLevel);
-  qr.addData(data);
+function generateImg(data) {
+  let qr;
+  let typeNumber = 1;
 
-  try {
-    qr.make();
-    break;
-  } catch(err) {
-    typeNumber += 1;
+  while (typeNumber <= 40) {
+    qr = qrcode(typeNumber, errorCorrectionLevel);
+    qr.addData(data);
+
+    try {
+      qr.make();
+      return qr.createImgTag(8, 8 * 4).match(/src="(.*?)"/)[1];
+    } catch(err) {
+      typeNumber += 1;
+    }
   }
 }
 
 const wrapper = document.createElement("div");
-wrapper.innerHTML = qr.createImgTag(8, 8 * 5);
 wrapper.style.position = "fixed";
 wrapper.style.zIndex = 2147483647;
 wrapper.style.top = 0;
@@ -30,13 +30,34 @@ wrapper.style.height = "100%";
 wrapper.style.backgroundColor = "rgba(255,255,255, 0.5)";
 wrapper.style.cursor = "pointer";
 wrapper.addEventListener("click", (event) => {
-  wrapper.parentNode.removeChild(wrapper);
+  if (event.target === event.currentTarget) {
+    wrapper.parentNode.removeChild(wrapper);
+  }
 }, false);
 
-const img = wrapper.getElementsByTagName("img")[0];
-img.style.position = "absolute";
-img.style.top = "50%";
-img.style.left = "50%";
-img.style.transform = "translate(-50%, -50%)";
+const qrBox = document.createElement("div");
+qrBox.style.position = "absolute";
+qrBox.style.top = "50%";
+qrBox.style.left = "50%";
+qrBox.style.transform = "translate(-50%, -50%)";
+qrBox.style.cursor = "auto";
+wrapper.appendChild(qrBox);
+
+const qrImg = document.createElement("img");
+qrImg.src = generateImg(location.href);
+qrImg.style.display = "block";
+qrBox.appendChild(qrImg);
+
+const input = document.createElement("input");
+input.type = "text";
+input.value = location.href;
+input.style.display = "block";
+input.style.width = "100%";
+input.style.fontSize = "14px";
+input.style.boxSizing = "border-box";
+input.addEventListener("input", (event) => {
+  qrImg.src = generateImg(input.value);
+}, false);
+qrBox.appendChild(input);
 
 document.body.appendChild(wrapper);
